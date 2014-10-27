9499759acd6aacb6023c2daa40169b5fbb2509b2
Veniamin Goldin <vgoldin@gmail.com>
2014-10-27 19:20:07 +0100

    Introduce helper RouteBuilder for REST services

---
+++ src/main/java/com/backbase/camel/hystrix/HystrixRestRouteBuilder.java
@@ -0,0 +1,83 @@
+package com.backbase.camel.hystrix;
+
+import static com.google.common.base.Objects.firstNonNull;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.HttpOperationFailedException;
+import org.apache.camel.spi.Language;
+
+public abstract class HystrixRestRouteBuilder extends RouteBuilder {
+    private final String basePath;
+
+    public HystrixRestRouteBuilder(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public Processor put(final String template) {
+        return rest(uri(template), "PUT");
+    }
+
+    public Processor delete(final String template) {
+        return rest(uri(template), "DELETE");
+    }
+
+    public Processor post(final String template) {
+        return rest(uri(template), "POST");
+    }
+
+    public Processor get(final String template) {
+        return rest(uri(template), "GET");
+    }
+
+    public static Processor rest(final String uri, final String verb) {
+        return new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Language language = exchange.getContext().resolveLanguage("simple");
+                Expression expression = language.createExpression(uri);
+                String uri = expression.evaluate(exchange, String.class);
+
+                exchange.getIn().setHeader(Exchange.HTTP_METHOD, firstNonNull(verb, "GET"));
+
+                Exchange response = exchange.getContext().createProducerTemplate().send(uri, exchange);
+
+                if (exchange.isFailed()) {
+                    Exception exception = exchange.getException();
+
+                    if (exception instanceof HttpOperationFailedException) {
+                        int statusCode = ((HttpOperationFailedException) exception).getStatusCode();
+
+                        if ((statusCode >= 500) && (statusCode <= 599)) {
+                            throw exception;
+                        } else {
+                            exchange.setException(null);
+                            exchange.getOut().setFault(false);
+
+                            exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, statusCode);
+                            exchange.getOut().setBody(((HttpOperationFailedException) exception).getResponseBody());
+                        }
+                    } else {
+                        throw exception;
+                    }
+                } else {
+                    exchange.setOut(response.getOut());
+                }
+            }
+        };
+    }
+
+    public String uri(String template) {
+        return basePath + template;
+    }
+
+    public Processor sync(Processor actualProcessor) {
+        return SyncHystrixCommandProcessor.sync(this.getClass().getSimpleName(), actualProcessor, null);
+    }
+
+    public Processor sync(Processor actualProcessor, Processor fallbackProcessor) {
+        return SyncHystrixCommandProcessor.sync(this.getClass().getSimpleName(), actualProcessor, fallbackProcessor);
+    }
+}
--- src/test/java/com/backbase/camel/hystrix/test/GoogleGeoCodesRoutes.java
+++ src/test/java/com/backbase/camel/hystrix/test/GoogleGeoCodesRoutes.java
@@ -1,44 +1,30 @@
 package com.backbase.camel.hystrix.test;
 
-import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.builder.RouteBuilder;
 
-import com.backbase.camel.hystrix.SyncHystrixCommandProcessor;
+import com.backbase.camel.hystrix.HystrixRestRouteBuilder;
 import com.netflix.hystrix.exception.HystrixRuntimeException;
 
-public class GoogleGeoCodesRoutes extends RouteBuilder {
-    Endpoint google;
+public class GoogleGeoCodesRoutes extends HystrixRestRouteBuilder {
+    public GoogleGeoCodesRoutes() {
+        super("http://bb-system-0205.local:9999/maps");
+    }
 
     public void configure() {
-        google = endpoint("http://bb-system-0205.local:9999/maps/api/geocode/json");
-
         from("quartz://geoCodes?trigger.repeatCount=10000&trigger.repeatInterval=300&fireNow=true")
             .routeId("GetGoogleGeoLocations")
             .setHeader("long",constant("40.714224"))
             .setHeader("lat",constant("-73.961452"))
             .setHeader(Exchange.HTTP_QUERY, simple("latlng=${headers.long},${headers.lat}"))
             .doTry()
-                .process(new SyncHystrixCommandProcessor(this.getClass().getSimpleName(),
-                        new GetGoogleGeoCodes(), new FallbackProcessor()))
+                .process(sync(get("/api/geocode/json"), new FallbackProcessor()))
             .doCatch(HystrixRuntimeException.class)
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
                 .setBody(constant("{\"error\" : \"Service is not available.\"}"))
             .end()
             .convertBodyTo(String.class)
             .log("${body}");
-    }
-
-    public class GetGoogleGeoCodes implements Processor {
-        @Override
-        public void process(Exchange exchange) throws Exception {
-            Object response = exchange.getContext().createProducerTemplate().requestBodyAndHeaders(google,
-                    null,
-                    exchange.getIn().getHeaders());
-
-            exchange.getIn().setBody(response);
-        }
     }
 
     private class FallbackProcessor implements Processor {
